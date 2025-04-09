c6e3b808d5620e825c525b9a5bfb1b52eaf9c966
Chamithree <you@example.com>
2025-04-09 10:43:25 -0400

    added logs

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -5,20 +5,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import utils.Log;
+
 public class BaseTest {
 	
 	protected WebDriver driver;
 	
 	@BeforeMethod
 	public void SetUp() {
+		Log.info("Starting Webdriver..");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		Log.info("Navigating to URL ..");
 		driver.get("https://admin-demo.nopcommerce.com/login");
 	}
 
 	@AfterMethod
 	public void TearDown() {
 		if(driver != null) {
+			Log.info("Closing Browser..");
 			driver.quit();
 		}
 	}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,13 +5,17 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.Log;
 
 public class LoginTest extends BaseTest {
 	
 	@Test
 	public void testValidLogin() {
 		
+		Log.info("Starting Login test..");
 		LoginPage loginPage = new LoginPage(driver);
+		
+		Log.info("Adding credentials..");
 		loginPage.enterUsername("admin@yourstore.com");
 		loginPage.enterPassword("admin");
 		loginPage.clickLogin();
